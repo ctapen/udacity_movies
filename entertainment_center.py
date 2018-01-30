81303b7eb747e84b371cda7d9a4365f9f5650838
ctapen <cmt78@georgetown.edu>
2018-01-30 06:57:28 -0500

    Add files via upload

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,30 +5,30 @@ import media
 # Create instances of Movie
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", # NOQA
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",  # NOQA
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg", # NOQA
+                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",  # NOQA
                      "http://www.youtube.com/watch?v=-9ceBgWV8io")
 
 BILB = media.Movie("Bend It Like Beckham",
                    "An indian girl living in UK who just wants to play "
                    "football.",
-                   "https://upload.wikimedia.org/wikipedia/en/1/1c/Bend_It_Like_Beckham_movie.jpg", # NOQA
+                   "https://upload.wikimedia.org/wikipedia/en/1/1c/Bend_It_Like_Beckham_movie.jpg",  # NOQA
                    "https://www.youtube.com/watch?v=Z7Pt_GMDdGo")
 
 school_of_rock = media.Movie("School of Rock", "Using rock music to learn.",
-                             "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg", # NOQA
+                             "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",  # NOQA
                              "https://www.youtube.com/watch?v=3PsUJFEBC74")
 
 ratatouille = media.Movie("Ratatoille", "A rat is a chef in Paris.",
-                          "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg", # NOQA
+                          "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",  # NOQA
                           "https://www.youtube.com/watch?v=c3sBBRxDAqk")
 
 hunger_games = media.Movie("Hunger Games", "A really real reality show.",
-                                "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg", # NOQA
+                                "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",  # NOQA
                                 "https://www.youtube.com/watch?v=4S9a5V9ODuY")
 
 # Make a list of Movies
--- media.py
+++ media.py
@@ -1,8 +1,11 @@
 import webbrowser
 # Create class Movie to hold movie info around title, storyline, image, trailer
+
+
 class Movie():
     """ This class Movie stores info about a movie."""
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
         """ This has variables about movie like title, trailer"""
@@ -10,6 +13,7 @@ class Movie():
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+
     def show_trailer(self):
         """ Creates capability to open trailer link via browser """
         webbrowser.open(self.trailer_youtube_url)
